4050bb73a478201eac7f9140d113669712bb260f
NiniPomelo <n0966785408@gmail.com>
2024-01-09 10:06:52 +0800

    week18

---
+++ week18/week18-1a.py
@@ -0,0 +1,4 @@
+#SOIT106_BACE_003
+a = list(map(int,input().split() ))
+
+print(sum(a)-a[0])
---
+++ week18/week18-1b.py
@@ -0,0 +1,8 @@
+# SOIT106_BACE_004
+a = int(input())
+if a<=2000: ans=100
+else:
+	ans = 100+(a-2000)//500*5
+	if (a-2000)%500>0: ans += 5
+
+print(ans)
